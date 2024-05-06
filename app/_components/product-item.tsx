1c1814a020c482515cd4663ee4451caf5cd38a11
WilliamGrohe <william.grohe@gmail.com>
2024-05-05 23:03:24 -0300

    add discount badge for product items

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -1,16 +1,17 @@
 import { Prisma, Product } from "@prisma/client";
 import Image from "next/image";
 import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
+import { ArrowDownIcon } from "lucide-react";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
     include: {
       restaurant: {
         select: {
-          name: true
-        }
-      }
-    }
+          name: true;
+        };
+      };
+    };
   }>;
 }
 
@@ -24,20 +25,30 @@ export function ProductItem({ product }: ProductItemProps) {
           className="rounded-lg object-cover shadow-md"
           fill
         />
+
+        {product.discountPercentage !== 0 && (
+          <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary px-2 text-white">
+            <ArrowDownIcon size={12} />
+            <span className="text-xs font-semibold">
+              {product.discountPercentage} %
+            </span>
+          </div>
+        )}
       </div>
+      
       <div className="">
-        <h2 className="text-sm truncate">{product.name}</h2>
+        <h2 className="truncate text-sm">{product.name}</h2>
         <div className="flex gap-1">
           <h3 className="font-semibold">
-           {formatCurrency(calculateProductTotalPrice(product))}
+            {formatCurrency(calculateProductTotalPrice(product))}
           </h3>
           {product.discountPercentage > 0 && (
-            <span className="text-muted-foreground text-xs line-through">
+            <span className="text-xs text-muted-foreground line-through">
               {formatCurrency(Number(product.price))}
             </span>
           )}
         </div>
-        <span className="text-muted-foreground text-xs block">
+        <span className="block text-xs text-muted-foreground">
           {product.restaurant.name}
         </span>
       </div>

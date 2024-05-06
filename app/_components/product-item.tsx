62c6b0e9cacc84091c971d5dad212d1aaffe19df
WilliamGrohe <william.grohe@gmail.com>
2024-05-06 00:43:32 -0300

    create page onf product details

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -2,6 +2,7 @@ import { Prisma, Product } from "@prisma/client";
 import Image from "next/image";
 import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
 import { ArrowDownIcon } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
@@ -17,41 +18,43 @@ interface ProductItemProps {
 
 export function ProductItem({ product }: ProductItemProps) {
   return (
-    <div className="w-[150px] min-w-[150px] space-y-2">
-      <div className="relative h-[150px] w-full">
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          className="rounded-lg object-cover shadow-md"
-          fill
-        />
+    <Link className="w-[150px] min-w-[150px]" href={`/products/${product.id}`}>
+      <div className="w-full space-y-2">
+        <div className="relative h-[150px] w-full">
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            className="rounded-lg object-cover shadow-md"
+            fill
+          />
 
-        {product.discountPercentage !== 0 && (
-          <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary px-2 text-white">
-            <ArrowDownIcon size={12} />
-            <span className="text-xs font-semibold">
-              {product.discountPercentage} %
-            </span>
-          </div>
-        )}
-      </div>
-      
-      <div className="">
-        <h2 className="truncate text-sm">{product.name}</h2>
-        <div className="flex gap-1">
-          <h3 className="font-semibold">
-            {formatCurrency(calculateProductTotalPrice(product))}
-          </h3>
-          {product.discountPercentage > 0 && (
-            <span className="text-xs text-muted-foreground line-through">
-              {formatCurrency(Number(product.price))}
-            </span>
+          {product.discountPercentage !== 0 && (
+            <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary px-2 text-white">
+              <ArrowDownIcon size={12} />
+              <span className="text-xs font-semibold">
+                {product.discountPercentage} %
+              </span>
+            </div>
           )}
         </div>
-        <span className="block text-xs text-muted-foreground">
-          {product.restaurant.name}
-        </span>
+
+        <div className="">
+          <h2 className="truncate text-sm">{product.name}</h2>
+          <div className="flex gap-1">
+            <h3 className="font-semibold">
+              {formatCurrency(calculateProductTotalPrice(product))}
+            </h3>
+            {product.discountPercentage > 0 && (
+              <span className="text-xs text-muted-foreground line-through">
+                {formatCurrency(Number(product.price))}
+              </span>
+            )}
+          </div>
+          <span className="block text-xs text-muted-foreground">
+            {product.restaurant.name}
+          </span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,81 @@
+import { Button } from "@/app/_components/ui/button";
+import {
+  calculateProductTotalPrice,
+  formatCurrency,
+} from "@/app/_helpers/price";
+import { db } from "@/app/_lib/prisma";
+import { ArrowDownIcon, ChevronLeftIcon } from "lucide-react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { ProductImage } from "./_components/product-image";
+import { DiscountBadge } from "@/app/_components/discount-badge";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div className="">
+      <ProductImage product={product} />
+
+      <div className="p-5">
+        {/* RESTAURANTE */}
+        <div className="flex items-center gap-[0.375rem]">
+          <div className="relative h-6 w-6">
+            <Image
+              src={product.restaurant.imageUrl}
+              alt={product.restaurant.name}
+              fill
+              className="rounded-full object-cover"
+            />
+          </div>
+          <span className="text-xs text-muted-foreground">
+            {product.restaurant.name}
+          </span>
+        </div>
+
+        {/* NOME DO PRODUTO */}
+        <h1 className="mb-3 mt-1 text-xl font-semibold">{product.name}</h1>
+
+        {/* PREÇO DO PRODUTO E QUANTIDADE */}
+        <div className="flex justify-between">
+          <div className="">
+            {/* PREÇO COM DESCONTO*/}
+            <div className="flex items-center gap-2">
+              <h2 className="text-xl font-semibold">
+                {formatCurrency(calculateProductTotalPrice(product))}
+              </h2>
+              {product.discountPercentage > 0 && (
+                <DiscountBadge product={product} />
+              )}
+            </div>
+            {/* PREÇO ORIGINAL */}
+            <p className="text-muted-foreground text-sm">
+              De: {formatCurrency(Number(product.price))}
+            </p>
+          </div>
+
+          {/* QUANTIDADE */}
+        </div>
+      </div>
+    </div>
+  );
+}
